d5023965d1ec235ac6c252e445c7387cc36704bd
Rodrigo Almeida <78742590+rpereiraalmeida@users.noreply.github.com>
2026-05-23 02:18:07 -0400

    Atualizar o script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API   = "https://rodrigopalmeida.pythonanywhere.com";
 const TOTAL = 250;
 const COLS  = 10;
 const ROWS  = 25;
-const ROW_LABELS = ["A","B","C","D","E","F","G","H","I","J"];
+const ROW_LABELS = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"];
 
 // ─── REFS ──────────────────────────────────────────────────────────
 const seatingArea  = document.getElementById("seating-area");
